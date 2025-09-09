7c93843b4c440539f3a4c15aba498a764bf50a2b
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-09 13:27:34 +0200

    extraction done

---
+++ lamaria/rig/keyframing/extraction.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from lamaria.rig.config.loaders import load_cfg
+from ...utils.utils import extract_images_from_vrs
+
+
+def run(
+    image_stream_folder: Path,
+    vrs_path: Path,
+) -> None:
+
+    image_stream_folder.mkdir(parents=True, exist_ok=True)
+    extract_images_from_vrs(
+        vrs_file=vrs_path,
+        image_folder=image_stream_folder,
+    )
+
+
+def main() -> None:
+    cfg = load_cfg()
+    run(
+        image_stream_folder=cfg.image_stream_path,
+        vrs_path=cfg.vrs_file_path,
+    )
+
+
+if __name__ == "__main__":
+    main()
