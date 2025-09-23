ece89f15ad889974f1411e35feb7e36d52c227a1
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-23 10:47:36 +0200

    general

--- lamaria/utils/general.py
+++ lamaria/utils/general.py
@@ -9,7 +9,6 @@ from bisect import bisect_left
 from typing import List
 import subprocess
 from scipy.spatial.transform import Rotation
-from decimal import Decimal, ROUND_HALF_UP
 
 from projectaria_tools.core.stream_id import StreamId
 from projectaria_tools.core import data_provider, mps
@@ -238,12 +237,6 @@ def camera_colmap_from_json(
         params=params,
     )
 
-def round_ns(x: str | int | float) -> int:
-    # works for "5120...274.999939", "5.12e11", or ints
-    if isinstance(x, int):
-        return x
-    s = str(x)
-    return int(Decimal(s).to_integral_value(rounding=ROUND_HALF_UP))
 
 def delete_files_in_folder(folder, exclude_pattern=None):
     if os.path.isdir(folder):
@@ -262,55 +255,6 @@ def delete_files_in_folder(folder, exclude_pattern=None):
                 print("Failed to delete %s. Reason: %s" % (file_path, e))
     else:
         os.makedirs(folder, exist_ok=True)
-
-def check_estimate_format(estimate_path: Path) -> bool:
-    """Estimate file format: ts t_x t_y t_z q_x q_y q_z q_w"""
-    with open(estimate_path, "r") as f:
-        lines = f.readlines()
-        lines = [line for line in lines if not line.startswith("#")]
-        if not lines:
-            raise ValueError(f"Estimate file {estimate_path} is empty \
-                             or has no valid lines.")
-
-        for line in lines:
-            parts = line.strip().split()
-            if len(parts) != 8:
-                raise ValueError(f"Estimate file {estimate_path} has invalid format. \
-                                 Each line must have 8 values.")
-            
-            try:
-                [float(part) for part in parts]
-            except ValueError:
-                raise ValueError(f"Estimate file {estimate_path} has invalid format. \
-                                 Each value must be a number.")
-    
-    return True
-
-def get_estimate_timestamps(estimate_path: Path) -> List[int]:
-    """Estimate file format: ts t_x t_y t_z q_x q_y q_z q_w"""
-    timestamps = []
-    with open(estimate_path, "r") as f:
-        lines = f.readlines()
-        lines = [line for line in lines if not line.startswith("#")]
-        if not lines:
-            raise ValueError(f"Estimate file {estimate_path} is empty \
-                             or has no valid lines.")
-
-        for line in lines:
-            parts = line.strip().split()
-            if len(parts) != 8:
-                raise ValueError(f"Estimate file {estimate_path} has invalid format. \
-                                 Each line must have 8 values.")
-            
-            try:
-                ts = round_ns(parts[0])
-            except ValueError:
-                raise ValueError(f"Estimate file {estimate_path} has invalid format. \
-                                 Each value must be a number.")
-            
-            timestamps.append(ts)
-        
-    return timestamps
 
 
 def extract_images_from_vrs(
@@ -362,35 +306,6 @@ def get_closed_loop_data_from_mps(mps_path: Path) -> List[mps.ClosedLoopTrajecto
     data = mps.read_closed_loop_trajectory(closed_loop_traj_file.as_posix())
     return data
 
-def get_rig_from_worlds_from_estimate(
-    estimate_path: Path
-) -> List[pycolmap.Rigid3d]:
-    """Estimate file format: ts t_x t_y t_z q_x q_y q_z q_w"""
-    
-    rig_from_worlds = []
-    with open(estimate_path, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            if line.startswith("#"):
-                continue
-            
-            parts = line.strip().split()
-            if len(parts) != 8:
-                raise ValueError(f"Estimate file {estimate_path} has invalid format. \
-                                 Each line must have 8 values.")
-
-            try:
-                tvec = np.array([float(parts[1]), float(parts[2]), float(parts[3])])
-                qvec = np.array([float(parts[4]), float(parts[5]), float(parts[6]), float(parts[7])])
-            except ValueError:
-                raise ValueError(f"Estimate file {estimate_path} has invalid format. \
-                                 Each value must be a number.")
-
-            pose = pycolmap.Rigid3d(pycolmap.Rotation3d(qvec), tvec)
-            rig_from_world = pose.inverse()
-            rig_from_worlds.append(rig_from_world)
-    
-    return rig_from_worlds
 
 def get_mps_poses_for_timestamps(
     trajectory_data: List[mps.ClosedLoopTrajectoryPose],
