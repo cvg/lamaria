990139376f52e7dc41ed7feb7a3ff741baa6204c
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-25 17:47:50 +0200

    sess

--- lamaria/rig/optim/session.py
+++ lamaria/rig/optim/session.py
@@ -6,33 +6,25 @@ from .imu import (
     load_imu_states,
     load_preintegrated_imu_measurements,
 )
-from ..config.options import VIOptimizerOptions
+from ...config.options import OptIMUOptions
 from ..lamaria_reconstruction import LamariaReconstruction
 
 class SingleSeqSession:
     def __init__(
         self,
-        options: VIOptimizerOptions,
+        imu_options: OptIMUOptions,
         data: LamariaReconstruction,
     ):
         self.data = data
-        self._init_options(options)
-        self._init_imu_data()
+        self._init_imu_data(imu_options)
 
-    def _init_imu_data(self):
+    def _init_imu_data(self, imu_options):
         self.preintegrated_imu_measurements = \
             load_preintegrated_imu_measurements(
-                self.imu_options,
+                imu_options,
                 self.data
             )
         self.imu_states = load_imu_states(self.data)
         self.imu_from_rig = pycolmap.Rigid3d()
         self.gravity = np.array([0.0, 0.0, -1.0])
         self.log_scale = np.array([0.0])
-
-    def _init_options(self, options: VIOptimizerOptions):
-        self.cam_options = options.cam
-        self.imu_options = options.imu
-        self.opt_options = options.optim
-
-
