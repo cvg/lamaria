74c075628360dfcd10cfdd3abc9e9c4fe9df5468
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-27 20:21:22 +0200

    constants.py

---
+++ lamaria/utils/constants.py
@@ -0,0 +1,16 @@
+
+from projectaria_tools.core.stream_id import StreamId
+
+# ASL folder name, stream label for Aria cameras
+ARIA_CAMERAS: list = [("cam0", "camera-slam-left"), 
+                      ("cam1", "camera-slam-right")]
+
+# Aria camera constants
+LEFT_CAMERA_STREAM_ID = StreamId("1201-1")
+RIGHT_CAMERA_STREAM_ID = StreamId("1201-2")
+LEFT_CAMERA_STREAM_LABEL = "camera-slam-left"
+RIGHT_CAMERA_STREAM_LABEL = "camera-slam-right"
+
+# Right Aria IMU constants
+RIGHT_IMU_STREAM_ID = StreamId("1202-1")
+RIGHT_IMU_STREAM_LABEL = "imu-right"
