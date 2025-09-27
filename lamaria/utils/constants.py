cba4d4a00c7fc99024cc7c9d552a406e78c964b8
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-27 20:29:25 +0200

    const

--- lamaria/utils/constants.py
+++ lamaria/utils/constants.py
@@ -14,3 +14,6 @@ RIGHT_CAMERA_STREAM_LABEL = "camera-slam-right"
 # Right Aria IMU constants
 RIGHT_IMU_STREAM_ID = StreamId("1202-1")
 RIGHT_IMU_STREAM_LABEL = "imu-right"
+
+# Custom origin coordinates (LV95 / LN02) for translating large CP coordinates
+CUSTOM_ORIGIN_COORDINATES = (2683594.4120000005, 1247727.7470000014, 417.307)
