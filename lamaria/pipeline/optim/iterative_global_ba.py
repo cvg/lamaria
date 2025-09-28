75791684425f9ee1d2ac8996a823d06aeefe2cba
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-28 21:18:35 +0200

    fix circular import

--- lamaria/pipeline/optim/iterative_global_ba.py
+++ lamaria/pipeline/optim/iterative_global_ba.py
@@ -5,7 +5,7 @@ from pycolmap import logging
 from ... import logger
 from ...config.options import VIOptimizerOptions
 from .callback import RefinementCallback
-from .imu import add_imu_residuals_to_problem
+from .residual import add_imu_residuals_to_problem
 from .session import SingleSeqSession
 
 
