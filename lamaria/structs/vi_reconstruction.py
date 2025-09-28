59da348749133ba37c7431a8c30a72b4860a894a
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-28 17:56:13 +0200

    vi recons

---
+++ lamaria/structs/vi_reconstruction.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+
+import numpy as np
+import pycolmap
+
+
+class VIReconstruction:
+    """Custom class to hold COLMAP reconstruction data along with
+    timestamps and IMU measurements.
+
+    Attributes:
+        reconstruction (pycolmap.Reconstruction):
+        The COLMAP reconstruction object. This object contains all the
+        3D points, cameras (intrinsics, extrinsics), and images.
+        Additionally holds a dummy camera, with no associated images,
+        to serve as a placeholder for the IMU.
+
+        timestamps (dict[int, int]): A dictionary mapping reconstruction
+        frame IDs to their corresponding timestamps in nanoseconds.
+
+        imu_measurements (pycolmap.ImuMeasurements): The factory/online
+        rectified IMU measurements generated from input VRS file.
+    """
+
+    def __init__(self) -> None:
+        self.reconstruction = pycolmap.Reconstruction()
+        self.timestamps: dict[int, int] = {}
+        self.imu_measurements = None
+
+    @staticmethod
+    def read(
+        input_folder: Path,
+    ) -> "VIReconstruction":
+        assert input_folder.exists(), (
+            f"Input folder {input_folder} does not exist"
+        )
+        instance = VIReconstruction()
+        instance.reconstruction = pycolmap.Reconstruction(input_folder)
+
+        ts_path = input_folder / "timestamps.txt"
+        assert ts_path.exists(), (
+            f"Timestamps file {ts_path} does not exist in {input_folder}"
+        )
+        with open(ts_path) as f:
+            lines = f.readlines()
+            instance.timestamps = {}
+            for line in lines:
+                if line.startswith("#"):
+                    continue
+                frame_id, ts = line.strip().split()
+                instance.timestamps[int(frame_id)] = int(ts)
+
+        rectified_imu_data_npy = input_folder / "rectified_imu_data.npy"
+        assert rectified_imu_data_npy.exists(), (
+            f"Rectified IMU data file {rectified_imu_data_npy} \
+                does not exist in {input_folder}"
+        )
+        rectified_imu_data = np.load(rectified_imu_data_npy, allow_pickle=True)
+        instance.imu_measurements = pycolmap.ImuMeasurements(
+            rectified_imu_data.tolist()
+        )
+
+        return instance
+
+    def write(
+        self,
+        output_folder: Path,
+    ) -> None:
+        output_folder.mkdir(parents=True, exist_ok=True)
+        self.reconstruction.write(output_folder.as_posix())
+
+        ts_path = output_folder / "timestamps.txt"
+        frame_ids = sorted(self.timestamps.keys())
+
+        # sanity check of frame ids in reconstruction and timestamps
+        recon_frame_ids = np.array(sorted(self.reconstruction.frames.keys()))
+        assert np.array_equal(np.array(frame_ids), recon_frame_ids), (
+            "Frame IDs in reconstruction and timestamps do not match"
+        )
+
+        with open(ts_path, "w") as f:
+            f.write("# FrameID Timestamp(ns)\n")
+            for frame_id in frame_ids:
+                f.write(f"{frame_id} {self.timestamps[frame_id]}\n")
+
+        rectified_imu_data_npy = output_folder / "rectified_imu_data.npy"
+        np.save(rectified_imu_data_npy, self.imu_measurements.data)
