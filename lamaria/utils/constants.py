e7ba58b957bbc85dde135cbc18997134dead5cb2
Anusha Krishnan <anushakrishnan3005@gmail.com>
2025-09-29 13:58:26 +0200

    rm stream id

--- lamaria/utils/constants.py
+++ lamaria/utils/constants.py
@@ -1,5 +1,3 @@
-from projectaria_tools.core.stream_id import StreamId
-
 # ASL folder name, stream label for Aria cameras
 ARIA_CAMERAS: list = [
     ("cam0", "camera-slam-left"),
@@ -7,13 +5,13 @@ ARIA_CAMERAS: list = [
 ]
 
 # Aria camera constants
-LEFT_CAMERA_STREAM_ID = StreamId("1201-1")
-RIGHT_CAMERA_STREAM_ID = StreamId("1201-2")
+LEFT_CAMERA_STREAM_ID = "1201-1"
+RIGHT_CAMERA_STREAM_ID = "1201-2"
 LEFT_CAMERA_STREAM_LABEL = "camera-slam-left"
 RIGHT_CAMERA_STREAM_LABEL = "camera-slam-right"
 
 # Right Aria IMU constants
-RIGHT_IMU_STREAM_ID = StreamId("1202-1")
+RIGHT_IMU_STREAM_ID = "1202-1"
 RIGHT_IMU_STREAM_LABEL = "imu-right"
 
 # Custom origin coordinates (LV95 / LN02) for translating large CP coordinates
